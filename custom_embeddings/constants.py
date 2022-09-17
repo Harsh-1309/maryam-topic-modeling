1319a3dbf83a81e759207069c3ab7a8a5c0f4e29
Harsh1309 <harsh13092001@gmail.com>
2022-09-18 02:41:56 +0530

    modify acquiring vectors

--- custom_embeddings/constants.py
+++ custom_embeddings/constants.py
@@ -2,10 +2,10 @@
 These constants will be used throughout the code base
 """
 
-DATASET_FILE ="data/text7"
+DATASET_FILE ="data/text6"
 PAD_STRING = "%s"
 WINDOW=3 #make it 3
-CONTEXT_MAP_PATH = "data/context_map_text7.json"
+CONTEXT_MAP_PATH = "data/context_map_text6.json"
 WINDOW_FLOAT = 3.0
 MAX_RATE = 8.0
 DIMENSION = 100 # initially dim was 100
@@ -16,5 +16,5 @@ WORDS=[]
 CONTEXT={} # This dict will store context map for entire dataset
 # VOCAB_SIZE=7 # was 14 for text6
 
-VOCAB_SIZE=627
-FINAL_DIM=VOCAB_SIZE*WINDOW*2*3
+VOCAB_SIZE=14
+# FINAL_DIM=VOCAB_SIZE*WINDOW*2*3
--- custom_embeddings/prepare_data.py
+++ custom_embeddings/prepare_data.py
@@ -49,35 +49,11 @@ for key,val in word2int.items():
 f = open(CONTEXT_MAP_PATH,"r")
 data = dict(json.load(f))
 
-# define one hot encoded padding vector
-padding_onehot=np.zeros(vocab_size)
-def add_padding():
-    return np.zeros(3)
+def modify_vector(base, multiplying_factor):
+    base=np.array(base)
+    multiplying_factor=np.array(multiplying_factor)
 
-#return true if left padding is not required
-def check_left_pad(word_ind,len):
-    if word_ind >= WINDOW:
-        return True
-    else:
-        return False
-
-#return true if right padding is not required
-def check_right_pad(word_ind,len):
-    if (len -1 - word_ind) >= WINDOW:
-        return True
-    else:
-        return False
-
-# returns padding count if required 
-def left_pad_count(word_ind,len):
-    return WINDOW-word_ind
-
-def right_pad_count(word_ind,len):
-    return WINDOW - (len -1 - word_ind)
-
-# get all indices
-def indices(lst, item):
-    return [i for i, x in enumerate(lst) if x == item]
+    return np.divide(np.multiply(base,multiplying_factor[1]),multiplying_factor[0])
 
 x=[]
 y=[]
@@ -86,143 +62,39 @@ with open(DATASET_FILE,"r") as f:
     # iterate line by line through dataset
     for line in tqdm(f):
         line=line.lower().strip().split()
-        for word in line:
-            context_=data[word]
+        for word in range(len(line)):
+            context_=data[line[word]]
 
-            # if words are repeated multiple times in same line
-            full_dataset_context=[]
-            # get all indices. Same word multiple times also works
-            word_index=indices(line,word)
-            line_len=len(line)
+            temp_x=[]
+            temp_y=[]
 
-            # iterate for each retrived index 
-            for wi in word_index:
-                full_context=[]
+            for w in range(1,WINDOW+1):
+                try:
 
-                # enter if if no padding required on both sides
-                if check_left_pad(wi,line_len)==True and check_right_pad(wi,line_len)==True:
-                    for i in range(WINDOW):
-                        full_context.append(line[wi-i-1]) #left context
-                        full_context.append(line[wi+i+1]) # followed by right context
-                    full_dataset_context.append(full_context)
+                    temp_x.append(modify_vector(one_hot[line[word+w]],context_[line[word+w]]))
+                    temp_y.append(one_hot[line[word]])
+                except:
+                    pass
+                try:
+                    if word-w>=0:
+                        temp_x.append(modify_vector(one_hot[line[word-w]],context_[line[word-w]]))
+                        temp_y.append(one_hot[line[word]])
+                except:
+                    pass
 
-                # enter this block when padding required on atleast 1 side
-                else:   
-
-                    left_check=check_left_pad(wi,line_len)
-                    right_check=check_right_pad(wi,line_len)
-
-                    # enter if padding on right is required
-                    if left_check == True:
-                        # rpc= required pad count
-                        rpc= right_pad_count(wi,line_len)
-                        avail_words=WINDOW-rpc
-                        for i in range(WINDOW):
-                            full_context.append(line[wi-i-1]) #left context
-
-                            if avail_words !=0:
-                                full_context.append(line[wi+i+1]) #right context
-                                avail_words-=1
-                            else:
-                                full_context.append(PAD_STRING)
-
-                    # enter if padding on left is required
-                    elif right_check == True:
-                        # rpc= required pad count
-                        rpc= left_pad_count(wi,line_len)
-                        avail_words=WINDOW-rpc
-                        for i in range(WINDOW):
-                            if avail_words !=0:
-                                full_context.append(line[wi-i-1]) #left context
-                                avail_words-=1
-                            else:
-                                full_context.append(PAD_STRING)
-
-                            full_context.append(line[wi+i+1]) #right context
-
-
-                        full_dataset_context.append(full_context)
-
-                    # enter if padding required on both sides
-                    else:
-                        rpc_right= right_pad_count(wi,line_len)
-                        rpc_left= left_pad_count(wi,line_len)
-                        avail_words_left=WINDOW-rpc_left
-                        avail_words_right=WINDOW-rpc_right
-
-                        for i in range(WINDOW):
-                            if avail_words_left !=0:
-                                full_context.append(line[wi-i-1]) #left context
-                                avail_words_left-=1
-                            else:
-                                full_context.append(PAD_STRING)
-
-                            if avail_words_right !=0:
-                                full_context.append(line[wi+i+1]) #right context
-                                avail_words_right-=1
-                            else:
-                                full_context.append(PAD_STRING)
-
-                        full_dataset_context.append(full_context)
-
-            # extend by context of all words
-            # each context will have a fixed size of WINDOW*2 
-            x.extend(full_dataset_context)
-            for _ in range(len(full_dataset_context)):
-                y.append(word)
-        
- 
-# replace the context strings with corresponding context matrix
-# and replace with one hot encoded vectors 
-x_context=[]
-y_context=[]
-
-x_onehot=[]
-y_onehot=[]
-
-for index,value in tqdm(enumerate(x)):
-    temp1=[]
-    temp2=[]
-    for k in value:
-        if k == PAD_STRING:
-            temp1.append([0,0,0])
-            temp2.append(list(padding_onehot))
-        else:
-            temp2.append(list(one_hot[k]))
-            if k != y[index]:
-                temp1.append(data[y[index]][k])
-            # word in self context is still an issue!!
-            else:
-                temp1.append([0,1,0])
-    x_context.append(temp1)
-    y_context.append(y[index])
-
-    x_onehot.append(temp2)
-    y_onehot.append(list(one_hot[y[index]]))
-
-# multiply one hot array and context map array
-# to get final data
-x_train=[]
-y_train=np.asarray(y_onehot,dtype="float64")
-
-# multiply element-wise
-for oneh,con in tqdm(zip(x_onehot,x_context)):
-    temp=[]
-    for j,k in zip(oneh,con):
-        multiplied_data=np.multiply.outer(np.array(j),np.array(k))
-        temp.append(multiplied_data)
-
-    # array flattened here to reduce complexity at this point
-    # ideally, it shouldn't be and will be changed later on to
-    # retain full shape for no distortion of information
-    x_train.append(np.array(temp).flatten())
-        
-x_train=np.asarray(x_train,dtype="float64")
-
-# save the data arrays to be used for training later on
-np.save("data/x_train.npy",x_train)
-np.save("data/y_train.npy",y_train)
+            x.extend(temp_x)
+            y.extend(temp_y)
 
 # save word2int to get final word embeddings after the training
 with open("data/word2int.json","w") as f:
     json.dump(word2int,f)
+
+with open("data/int2word.json","w") as f:
+    json.dump(int2word,f)
+
+x_train=np.asarray(x,dtype="float64")
+y_train=np.asarray(y,dtype="float64")
+
+# save the data arrays to be used for training later on
+np.save("data/x_train.npy",x_train)
+np.save("data/y_train.npy",y_train)
